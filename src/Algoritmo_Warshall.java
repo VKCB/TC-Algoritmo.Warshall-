ce86af1d9aa66d044702b70d2abcd4511d028ac5
Dylan Garbanzo <dgarbanzof@estudiantec.cr>
2021-11-07 18:11:30 -0600

    Warhsall arreglado

--- src/Algoritmo_Warshall.java
+++ src/Algoritmo_Warshall.java
@@ -1,75 +1,47 @@
-import java.util.Scanner;
-
-/** Class Warshall **/
-class Warshall
-{
-    private int V;
-    private boolean[][] tc;
-    /** Function to make the transitive closure **/
-    public void getTC(int[][] graph)
-    {
-        this.V = graph.length;
-        tc = new boolean[V][V];
-        for (int i = 0; i < V; i++)
-        {
-            for (int j = 0; j < V; j++)
-                if (graph[i][j] != 0)
-                    tc[i][j] = true;
-            tc[i][i] = true;
-        }
-        for (int i = 0; i < V; i++)
-        {
-            for (int j = 0; j < V; j++)
-            {
-                if (tc[j][i])
-                    for (int k = 0; k < V; k++)
-                        if (tc[j][i] && tc[i][k])
-                            tc[j][k] = true;
+class Warshall {
+    public static void procesoWarshall(int a[][]) {
+        int n = a.length;
+        for (int k = 0; k < n; k++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (a[i][j] != 1) {
+                        a[i][j] = a[i][k] & a[k][j];
+                    }
+                }
             }
         }
     }
-    /** Funtion to display the trasitive closure **/
-    public void displayTC()
-    {
-        System.out.println("\nTransitive closure :\n");
-        System.out.print(" ");
-        for (int v = 0; v < V; v++)
-            System.out.print("   " + v );
-        System.out.println();
-        for (int v = 0; v < V; v++)
-        {
-            System.out.print(v +" ");
-            for (int w = 0; w < V; w++)
-            {
-                if (tc[v][w])
-                    System.out.print("  * ");
-                else
-                    System.out.print("    ");
+
+
+    public static void MostrarMatrizClausura(int[][] a) {
+        int n = a.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(" " + a[i][j]);
             }
-            System.out.println();
+            System.out.println(" ");
         }
     }
+}
 
-    /** Main function **/
-    public static void main (String[] args)
-    {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Warshall Algorithm Test\n");
-        /** Make an object of Warshall class **/
-        Warshall w = new Warshall();
+public class Algoritmo_Warshall {
+    public static void main(String[] args) {
+        int x[][];
+        int n;
+        x = new int[4][4];
+        n = x.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                x[i][j]=0;
 
-        /** Accept number of vertices **/
-        System.out.println("Enter number of vertices\n");
-        int V = scan.nextInt();
+            }
+        }
+        x[0][1] = 1;
+        x[1][0] = 1;
+        x[1][2] = 1;
+        x[2][3] = 1;
 
-        /** get graph **/
-        System.out.println("\nEnter matrix\n");
-        int[][] graph = new int[V][V];
-        for (int i = 0; i < V; i++)
-            for (int j = 0; j < V; j++)
-                graph[i][j] = scan.nextInt();
-
-        w.getTC(graph);
-        w.displayTC();
+        Warshall.procesoWarshall(x);
+        Warshall.MostrarMatrizClausura(x);
     }
 }
